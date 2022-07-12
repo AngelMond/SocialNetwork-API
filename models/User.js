3747b75895d10812fbdd07106a1b4b78fc7bd1b2
AngelMond <abraham_will@hotmail.com>
2022-07-12 00:34:19 -0500

    Added match property to email field in userSchema to match valid email address

--- models/User.js
+++ models/User.js
@@ -2,22 +2,35 @@ const mongoose = require('mongoose');
 
 
 //User Schema
-const User = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
 
-    username: {type: String, unique: true, required: true, trimmed: true},
+    username: {type: String, unique: true, required: true, trim: true},
 
-    email: {type: String, required: true, unique: true, maxLength: 20},//falta añadir validacion, debe hacer match con un email valido:  (look into Mongoose's matching validation)
+    email: {
+        type: String, 
+        required: [true, 'Please enter an email adress'],
+        unique: true, 
+        match: [
+            /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'Please enter a valid email address.',
+        ]
+    },
     
-    thoughts: [Thought], //Debe de contener el esquema de Thought
+    thoughts: [thoughtSchema], //Hace referencia al modelo Thought
 
-    friends: [User],//Debe hacer referencia asi mismo
-
-
-    //Create a virtual called `friendCount` that retrieves the length of the user's `friends` array field on query.
+    friends: [userSchema],//Hace referencia asi mismo
+    
 });
 
+//Create a virtual called `friendCount` that retrieves the length of the user's `friends` array field on query.
+userSchema.virtual('friendCount').get(function(){
+    return this.friends.length;
+});
+
+
+
 //Thought Schema
-const Thought = new mongoose.Schema({
+const thoughtSchema = new mongoose.Schema({
     thoughtText: {type: String, required: true, maxLength: 20},//Must be between 1 and 280 characters buscar delimiter filter o meter regex
 
     createdAt: {type: Date, default: Date.now}, //Use a getter method to format the timestamp on query
@@ -34,7 +47,7 @@ const Thought = new mongoose.Schema({
 
 //This will not be a model, but rather will be used as the `reaction` field's subdocument schema 
 //in the `Thought` model. (ONLY SCHEMA)
-const Reaction = new mongoose.Schema({
+const reactionSchema = new mongoose.Schema({
     reactionId: {},
 
     reactionBody: {type:String, required: true, maxLength: 20 }, //Anadir 280 characters como maximo
